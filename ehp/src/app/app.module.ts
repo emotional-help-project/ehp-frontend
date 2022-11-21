0c888abc3437b88d19050f0e0dede80fd1735724
Nataliia Chapai <natalya.chapay@gmail.com>
2022-11-21 20:52:31 +0200

    get started

---
+++ ehp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './features/components/auth/auth.component';
+import { HomeComponent } from './features/components/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
