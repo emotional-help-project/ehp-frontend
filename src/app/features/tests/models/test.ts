15657befa47a86de0cb87ceff428f8b5fd2b4c1b
Nataliia Chapai <natalya.chapay@gmail.com>
2022-11-23 01:56:01 +0200

    add test list component

---
+++ src/app/features/tests/models/test.ts
@@ -0,0 +1,5 @@
+export interface Test {
+    name: string;
+    description: string;
+    imgUrl: string;
+}
--- src/app/features/tests/tests.module.ts
+++ src/app/features/tests/tests.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TestSectionComponent } from './components/test-section/test-section.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TestListComponent } from './components/test-list/test-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -12,11 +14,13 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    TestSectionComponent
+    TestSectionComponent,
+    TestListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class TestsModule { }
