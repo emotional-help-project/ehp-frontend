69ef84b6d9f47c68d3bfd281148c4bc69854a474
Tetiana <yankiv1998@gmail.com>
2022-11-23 20:30:14 +0200

    wip

---
+++ src/app/shared/courses/courses.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor() { }
+
+  public courses = [
+    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
+    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
+    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '}
+
+  ]
+}
