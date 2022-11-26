d234ca0031cd22c006f0039c42744123ed41cf00
Tetiana <yankiv1998@gmail.com>
2022-11-26 12:04:06 +0200

    done main page

--- src/app/shared/courses/courses.service.ts
+++ src/app/shared/courses/courses.service.ts
@@ -8,9 +8,9 @@ export class CoursesService {
   constructor() { }
 
   public courses = [
-    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
-    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
-    {url:'', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '}
+    {url:'../../../assets/imges/feelings.png', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
+    {url:'../../../assets/imges/feelings.png', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '},
+    {url:'../../../assets/imges/feelings.png', title: 'Mental health training', description: 'Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. '}
 
   ]
 }
