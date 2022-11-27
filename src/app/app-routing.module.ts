8b08aa2e6c92f4c4cfa3e3d886c040b12b93a92c
Nataliia Chapai <natalya.chapay@gmail.com>
2022-11-28 00:19:36 +0200

    protect tests path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
     path: 'tests',
     loadChildren: () => import('./features/tests/tests.module').then(_ => _.TestsModule)
   },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
 ];
 
 @NgModule({
