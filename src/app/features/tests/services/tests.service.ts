8a6473681367e4ef9ac148bda74d11c6e2dffb32
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-03 00:38:07 +0200

    edit messenger button

--- src/app/features/tests/services/tests.service.ts
+++ src/app/features/tests/services/tests.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map, catchError, throwError, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  map,
+  catchError,
+  throwError,
+  tap,
+} from 'rxjs';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 import { MessagesService } from 'src/app/shared/services/messages.service';
 import { environment } from 'src/environment/environment';
@@ -9,7 +16,7 @@ import { TestListItem } from '../models/test-list-item.interface';
 import { Test } from '../models/test.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TestsService {
   private testListSubject = new BehaviorSubject<TestListItem[]>([]);
@@ -22,60 +29,62 @@ export class TestsService {
   public test: Test = {
     totalNumberOfTestQuestions: 2,
     items: [
-        {
-            questionId: 1,
-            allowsMultipleAnswers: false,
-            questionText: "Do you suffer from extreme mood changes (e.g. going from extremely \"happy\" to extremely \"sad\")?",
-            answers: [
-                {
-                    answerId: 1,
-                    answerText: "Yes",
-                    checked: false
-                },
-                {
-                    answerId: 2,
-                    answerText: "Sometimes",
-                    checked: false
-                },
-                {
-                    answerId: 3,
-                    answerText: "No",
-                    checked: false
-                }
-            ]
-        },     
-        {
-          questionId: 2,
-          allowsMultipleAnswers: true,
-          questionText: "Has anyone in your family ever been diagnosed with Bipolar Disorder?",
-          answers: [
-              {
-                  answerId: 4,
-                  answerText: "Yes",
-                  checked: false
-              },
-              {
-                  answerId: 5,
-                  answerText: "Sometimes",
-                  checked: false
-              },
-              {
-                  answerId: 6,
-                  answerText: "No",
-                  checked: false
-              }
-          ]
-      }
-  ]
-}
+      {
+        questionId: 1,
+        allowsMultipleAnswers: false,
+        questionText:
+          'Do you suffer from extreme mood changes (e.g. going from extremely "happy" to extremely "sad")?',
+        answers: [
+          {
+            answerId: 1,
+            answerText: 'Yes',
+            checked: false,
+          },
+          {
+            answerId: 2,
+            answerText: 'Sometimes',
+            checked: false,
+          },
+          {
+            answerId: 3,
+            answerText: 'No',
+            checked: false,
+          },
+        ],
+      },
+      {
+        questionId: 2,
+        allowsMultipleAnswers: true,
+        questionText:
+          'Has anyone in your family ever been diagnosed with Bipolar Disorder?',
+        answers: [
+          {
+            answerId: 4,
+            answerText: 'Yes',
+            checked: false,
+          },
+          {
+            answerId: 5,
+            answerText: 'Sometimes',
+            checked: false,
+          },
+          {
+            answerId: 6,
+            answerText: 'No',
+            checked: false,
+          },
+        ],
+      },
+    ],
+  };
 
   constructor(
     private loader: LoadingService,
     private messages: MessagesService,
     private http: HttpClient,
-    private user: LoginService,
+    private user: LoginService
   ) {
-    this.user.user$.pipe(map(user => this.userId = user?.userId));
+    this.user.user$.pipe(map(user => (this.userId = user?.userId)));
 
     if (localStorage.getItem('token')) {
       this.loadAllTests();
@@ -83,7 +92,8 @@ export class TestsService {
   }
 
   public loadAllTests() {
-    const url = environment.apiUrl + `/tests/user/${this.userId}?skip=0&take=10`;
+    const url =
+      environment.apiUrl + `/tests/user/${this.userId}?skip=0&take=10`;
     const loadTests$ = this.http.get<any>(url).pipe(
       map(res => res.tests.content),
       catchError(err => {
@@ -98,6 +108,6 @@ export class TestsService {
   }
 
   finishTest() {
-    console.log();  
+    console.log();
   }
 }
