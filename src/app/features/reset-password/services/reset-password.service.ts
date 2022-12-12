0893adddb52688c00c666ba4f838f0771a910359
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-12 19:04:23 +0200

    bugfix

--- src/app/features/reset-password/services/reset-password.service.ts
+++ src/app/features/reset-password/services/reset-password.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ResetPasswordService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ResetPasswordService {
 
-  constructor() { }
-}
+//   constructor() { }
+// }
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl: 'http://5.58.12.93:8095/api'
-  };
+  // apiUrl: 'http://5.58.12.93:8095/api'
+  apiUrl: 'http://52.89.90.94:8080/api'
+};
