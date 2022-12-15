f4b88a80735b49d2b08d3d59e64559cc5c8124cd
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-15 11:20:59 +0200

    edit home page

--- src/app/features/home/courses/courses.component.ts
+++ src/app/features/home/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { map, Observable } from 'rxjs';
 import { CoursesService } from 'src/app/shared/courses/courses.service';
+import { LoginService } from '../../login/services/login.service';
 import { Course } from '../../tests/models/course.model';
 
 @Component({
@@ -10,10 +12,39 @@ import { Course } from '../../tests/models/course.model';
 })
 export class CoursesComponent implements OnInit {
 
+  isAdmin = this.loginService.getParsedToken()?.isAdmin;
   courses$: Observable<Course[]>
 
-constructor(public coursesService: CoursesService){}
+constructor(public coursesService: CoursesService, private dialog: MatDialog, public loginService: LoginService){}
   ngOnInit(): void {
+    this.getAllCourses()
     this.courses$ = this.coursesService.getCourses().pipe(map(res => res));
+  }
+
+  openAddDialog() {
+    this.coursesService.coursesDialog({
+      title: 'Add new training',
+      btn: 'Save'
+    });
+  }
+  getAllCourses(){
+    this.coursesService.getCourses()
+    .subscribe({
+      next:(res)=>{
+        console.log(res)
+      }
+    })
+  }
+  openUpdateDialog(course: any){
+    this.coursesService.coursesDialog({
+      title: 'Update training',
+      btn: 'Update',
+      course
+    });
+  
+  }
+  onRemove(id: any){
+    this.coursesService.delete(id)
+    .subscribe()
   }
 }
